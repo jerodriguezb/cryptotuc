1348e98a0041fb0411c8b2e4bf1935f3001c64a1
Federico <federicorg07@gmail.com>
2022-09-22 21:10:28 -0300

    features-initial visual calculator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import Calculadora from './pages/Calculadora';
 
 const App = () => {
   return (
-    <div>Esto es APP</div>
+    <>
+    <div className='bg-black'>
+      <Header />
+    <Calculadora />
+    </div>
+    </>
   );
 };
 
