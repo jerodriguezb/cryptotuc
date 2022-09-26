9cabb383e0b4492cc7cc19fb77d30f6d17b56194
Federico <federicorg07@gmail.com>
2022-09-26 13:42:28 -0300

    feature styles and changes page calculator

--- src/pages/Calculator/Calculator.js
+++ src/pages/Calculator/Calculator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch/useFetch';
 
 const Calculator = () => {
@@ -23,7 +24,7 @@ const Calculator = () => {
 
   return (
     <>
-    <div className="container my-5">
+    {/* <div className="container my-5">
         <div className="row">
           <div className="col-lg-10 mx-auto m-20">
             <div className="card card-body">
@@ -35,53 +36,38 @@ const Calculator = () => {
               </h4>
               <div className="row">
                 <div className="col-lg-10">
-
-                  {/* <form className="form-inline mb-4 d-flex">
-                    <input
-                      type="number"
-                      className="form-control form-control-lg mx-3"
-                    />
-                        <select name="">
-                            <option value="">Seleccione</option>
-                            <option value="">BTC</option>
-                            <option value="">ETH</option>
-                        </select>
-                  </form>
-
                   <form className="form-inline mb-4 d-flex">
-                    <input
-                      className="form-control form-control-lg mx-3"
-                    />
-                    <select name="">
-                            <option value="">Seleccione</option>
-                            <option value="">ARS</option>
-                            <option value="">USD</option>
-                        </select>
-                  </form> */}
-
-                  <form className="form-inline mb-4 d-flex">
-                      {Rates?.loading && <h2> Loading ... Rates...</h2>};
-                      <select name="select-rates" id= "select-rates" onChange={(e) => handleChangeSelect(e)}>
+                      {Rates?.loading && <h2> Loading ... Rates...</h2>}
+                      <select name="select-rates" id= "select-rates"
+                      onChange={(e) => handleChangeSelect(e)}>
                         {!Rates?.loading && Rates?.data?.data?.map(rate => <option key={rate.id}
                         value={rate.rateUsd}>{rate.id}</option>)}
                       </select>
+                  </form>
 
-                      {Coins?.loading && <h2> Loading ... Coins...</h2>};
-                      <select name="select-coins" id="select-coins" onChange={(e) => handleChangeSelect(e)}>
+                      {Coins?.loading && <h2> Loading ... Coins...</h2>}
+                      <select name="select-coins" id="select-coins"
+                       onChange={(e) => handleChangeSelect(e)}>
                         {!Coins?.loading && Coins?.data?.data?.map(coin => <option key={coin.id}
                         value={coin.priceUsd}>{coin.id}</option>)}
                       </select>
 
-                      <div>
-                          <label> Ingresa un Valor: </label>
-                          <input className="form-control form-control-lg mx-3" type="text" id="form-valor" name="form-valor"
+                      <div className='d-flex'>
+                        <form action="">
+                            <label> Ingresa un Valor: </label>
+                          <input className="form-control form-control-lg mx-3"
+                          type="text" id="form-valor" name="form-valor"
                           onChange={(e) => handleChange(e)}/>
-                        </div>
+                        </form>
+                      </div>
                         <div>
-                          <label> Valor Calculado: </label>
-                          <input className="form-control form-control-lg mx-3" type="text" id="form-calculator" name="form-calculator" readOnly="readonly"/>
+                          <form action="">
+                              <label> Valor Calculado: </label>
+                          <input className="form-control form-control-lg mx-3"
+                          type="text" id="form-calculator"
+                          name="form-calculator" readOnly="readonly"/>
+                          </form>
                         </div>
-                    </form>
                 </div>
 
                   <div className="col-lg-2 align-self-center">
@@ -92,6 +78,47 @@ const Calculator = () => {
             </div>
           </div>
         </div>
+      </div> */}
+      <div className="container my-5 bg-dark w-50 text-light p-2">
+                        <h3 className='text-center'>
+                            <i className="bi bi-calculator"></i>
+                        </h3>
+                        <h5 className='text-center'>Realiza el calculo moneda a tu cripto favorita</h5>
+                    <form className="row g-3 p-3">
+                      <div className="col-md-6">
+                        <label className="form-label">Seleccione moneda</label>
+                           {Rates?.loading && <h2>Loading...</h2>}
+                        <select name='select-rates' id="select-rates" className="form-select"
+                        onChange= {(e) => handleChangeSelect(e)}>
+                          {!Rates?.loading && Rates?.data?.data?.map(rate => <option key={rate.id}
+                            value={rate.rateUsd}>{rate.id}</option>)}
+                        </select>
+                    </div>
+                      <div className="col-md-6">
+                       <label className="form-label">Ingresa un Valor</label>
+                        <input onChange={(e) => handleChange(e)}
+                        type="text" className="form-control form-control-md" id="form-valor" name='form-valor'/>
+                      </div>
+                      <div className="col-md-6">
+                        <label className="form-label">Seleccione cripto</label>
+                        {Coins?.loading && <h2> Loading ... Rates... </h2>}
+                        <select name='select-coins' id="select-coins" className="form-select"
+                        onChange= {(e) => handleChangeSelect(e)}>
+                          {!Coins?.loading && Coins?.data?.data?.map(coin => <option key={coin.id}
+                            value={coin.priceUsd}>{coin.id}</option>)}
+                        </select>
+                    </div>
+                      <div className="col-md-6">
+                       <label className="form-label">Valor calculado</label>
+                        <input className="form-control form-control-md" type="text"
+                        id="form-calculator" name="form-calculator" readOnly="readonly"/>
+                      </div>
+                      <NavLink className='nav-link text-center' to='/'>
+                          <button className='btn btn-primary bg-dark border-0'>
+                            <i className="bi bi-arrow-left"></i>
+                         </button>
+                      </NavLink>
+                  </form>
       </div>
     </>
   );
