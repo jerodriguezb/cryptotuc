6143f750096343f66d0cc32df975a6d73fde204e
Juan Carlos Martinez Issa <carloswolvi@gmail.com>
2022-09-27 16:23:57 -0300

    barra busqueda incompleto

--- src/components/Layout/CriptoList/CriptoList.js
+++ src/components/Layout/CriptoList/CriptoList.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import Items from './Items';
-import useFetch from './useFetch';
 
-const CriptoList = () => {
-  const datos = useFetch('https://api.coincap.io/v2/assets');
-  console.log(datos);
-  return (
+const CriptoList = ({ datos }) => {
+return (
     <ol className="list-group list-group-numbered">
       {!datos?.loading && datos.data.data.slice(0, 10).map(crypto => (
         <Items
-        key={crypto.id}
-        name={crypto.name}
-        symbol={crypto.symbol}
-        price={crypto.priceUsd}
+          key={crypto.id}  
+          name={crypto.name}
+          symbol={crypto.symbol}
+          price={crypto.priceUsd}
         />
       ))}
     </ol>
--- src/components/Layout/SearchBox/SearchBox.js
+++ src/components/Layout/SearchBox/SearchBox.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useFetch } from '../../../Hooks';
 
 const SearchBox = () => {
+
+  const [criptos, setCriptos]= useState([]);
+  const [tablaCripto, setTablaCripto]= useState([]);
+  const [busqueda, setBusqueda]= useState("");
+
+
+  
   return (
     <div className="form">
       <input type="text" className="form-control form-input" placeholder="Buscar"></input>
