5ffe81eb678c8f0e6e69762e9912882fd8d48b99
Jorge <91227064+JRSalica@users.noreply.github.com>
2022-10-02 11:41:33 -0300

    Revert "Merge branch 'master' of https://github.com/jerodriguezb/cryptotuc into ft/home-header-tests"
    This reverts commit a95f48b0b1d2baffa6af6e69fc16466897e52216, reversing
    changes made to fbb844d92a83c67289423c93ac7b202edc0fd9dc.

--- src/pages/Calculator/Calculator.js
+++ src/pages/Calculator/Calculator.js
@@ -1,16 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import classNames from 'classnames';
-import { useSelector } from 'react-redux';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch/useFetch';
 
 const Calculator = () => {
-  const { coin } = useSelector((state) => state.coin);
-  const { theme } = useSelector((state) => state.theme);
   const Coins = useFetch('https://api.coincap.io/v2/assets');
   const Rates = useFetch('https://api.coincap.io/v2/rates');
-  const [changeCoin, setchangeCoin] = useState(coin);
-
   const handleChange = (e) => {
     const regex = /^[0-9]*$/;
     const onlyNumbers = regex.test(e.target.value);
@@ -28,53 +22,27 @@ const Calculator = () => {
     }
   };
 
-  useEffect(() => {
-    setchangeCoin(coin);
-    handleChangeSelect();
-  }, [coin, changeCoin]);
-
   return (
     <>
-      <div className={classNames('container my-5 w-50 text-light p-2', {
-        'bg-gray-300': theme === 'light',
-        'bg-dark': theme === 'dark',
-      })}>
-        <h2 className={classNames('text-center', {
-          'text-dark': theme === 'light',
-          'text-white': theme === 'dark',
-        })}>CryptoTuc Calculadora</h2>
-        <h4 className='text-center'>
-          <i className={classNames('bi bi-calculator', {
-            'text-dark': theme === 'light',
-            'text-white': theme === 'dark',
-          })}></i>
-        </h4>
-        <h5 className={classNames('text-center', {
-          'text-dark': theme === 'light',
-          'text-white': theme === 'dark',
-        })}>Realiza el cálculo moneda a tu cripto favorita</h5>
+      <div className="container my-5 bg-dark w-50 text-light p-2">
+        <h3 className='text-center'>
+          <i className="bi bi-calculator"></i>
+        </h3>
+        <h5 className='text-center'>Realiza el cálculo moneda a tu cripto favorita</h5>
         <form className="row g-3 p-3">
           <div className="col-md-6">
-            <label className={classNames('form-label', {
-              'text-dark': theme === 'light',
-              'text-white': theme === 'dark',
-            })}>Seleccione moneda</label>
+            <label className="form-label">Seleccione moneda</label>
             {Rates?.loading && <div className="spinner-grow text-info spinner-grow-sm ms-2" role="status">
               <span className="visually-hidden">Loading...</span>
             </div>}
-
             <select name='select-rates' id="select-rates" className="form-select"
-              onChange={() => handleChangeSelect()}>
+              onChange={(e) => handleChangeSelect(e)}>
               {!Rates?.loading && Rates?.data?.data?.map(rate => <option key={rate.id}
-                value={rate.rateUsd} selected={rate.id === changeCoin}>{rate.id}</option>)}
+                value={rate.rateUsd}>{rate.id}</option>)}
             </select>
-
           </div>
           <div className="col-md-6">
-            <label className={classNames('form-label', {
-              'text-dark': theme === 'light',
-              'text-white': theme === 'dark',
-            })}>Ingrese Valor</label>
+            <label className="form-label">Ingresa un Valor</label>
             <input onChange={(e) => handleChange(e)}
               type="text" className="form-control form-control-md" id="form-valor" name='form-valor' />
           </div>
@@ -83,29 +51,21 @@ const Calculator = () => {
             {Coins?.loading && <div className="spinner-grow text-info spinner-grow-sm ms-2" role="status">
               <span className="visually-hidden">Loading...</span>
             </div>}
-
             <select name='select-coins' id="select-coins" className="form-select"
-              onChange={() => handleChangeSelect()}>
-              {!Coins?.loading && Coins?.data?.data?.map(currency => <option key={currency.id}
-                value={currency.priceUsd}>{currency.id}</option>)}
+              onChange={(e) => handleChangeSelect(e)}>
+              {!Coins?.loading && Coins?.data?.data?.map(coin => <option key={coin.id}
+                value={coin.priceUsd}>{coin.id}</option>)}
             </select>
-
           </div>
           <div className="col-md-6">
-            <label className={classNames('form-label', {
-              'text-dark': theme === 'light',
-              'text-white': theme === 'dark',
-            })}>Valor calculado</label>
+            <label className="form-label">Valor calculado</label>
             <input className="form-control form-control-md" type="text"
               id="form-calculator" name="form-calculator" readOnly="readonly" />
           </div>
           <NavLink className='nav-link text-center' to='/'>
-            <h4 className=''>
-              <i className={classNames('bi bi-arrow-left', {
-                'text-dark': theme === 'light',
-                'text-white': theme === 'dark',
-              })}></i>
-            </h4>
+            <button className='btn btn-primary bg-dark border-0'>
+              <i className="bi bi-arrow-left"></i>
+            </button>
           </NavLink>
         </form>
       </div>
