a4b8f7ce81f7b233288ebec5cdbcf8d5f5f1c3c5
Jorge <91227064+JRSalica@users.noreply.github.com>
2022-10-03 19:07:10 -0300

    firsts test

--- src/components/Layout/Header/Header.test.js
+++ src/components/Layout/Header/Header.test.js
@@ -12,22 +12,12 @@ describe('Header Test', () => {
   });
 
   test('Deberia renderizar el componente Header con theme Dark', () => {
-    const setDefault = jest.fn();
+    const setThemeDefault = jest.fn();
 
     renderWithProviders(<BrowserRouter><Header /></BrowserRouter>);
     const element = screen.getByTestId('theme-switch');
     fireEvent.click(element);
 
-    expect(setDefault).toHaveBeenCalled();
-  });
-
-  test('Deberia renderizar el componente Header con theme Light', () => {
-    const setLight = jest.fn();
-
-    renderWithProviders(<BrowserRouter><Header /></BrowserRouter>);
-    const element = screen.getByTestId('theme-switch');
-    fireEvent.click(element);
-
-    expect(setLight).toHaveBeenCalled();
+    expect(setThemeDefault).toHaveBeenCalled();
   });
 });
