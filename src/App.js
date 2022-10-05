db248f4cebd8d04521a525344865a677977ba371
Jorge <91227064+JRSalica@users.noreply.github.com>
2022-10-05 17:14:24 -0300

    styles refactor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import classNames from 'classnames';
+import { useSelector } from 'react-redux';
 import { Header, Main, Footer } from './components/Layout';
 import './scss/custom.scss';
 
 const App = () => {
+  const { theme } = useSelector((state) => state.theme);
   return (
-    <>
+    <div className={classNames('app-container py-0', {
+      'bg-gray-400': theme === 'light',
+      'bg-gray-200': theme === 'dark',
+    })}>
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 };
 
--- src/components/CoinHistory/CoinHistory.js
+++ src/components/CoinHistory/CoinHistory.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Line } from 'react-chartjs-2';
 import {
@@ -11,10 +11,22 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
+import classNames from 'classnames';
+import { useSelector } from 'react-redux';
 import useFetch from '../../hooks/useFetch/useFetch';
 
-const CoinHistory = ({ idCoin }) => {
-  const moneda = useFetch(`https://api.coincap.io/v2/assets/${idCoin}/history?interval=h1`);
+const CoinHistory = () => {
+  const { theme } = useSelector((state) => state.theme);
+
+  const [allCoins, setAllCoins] = useState([]);
+  const [selectedCoin, setSelectedCoin] = useState(useSelector((state) => state.coin));
+  const [allCryptos, setAllCryptos] = useState([]);
+  const [selectedCrypto, setSelectedCrypto] = useState([]);
+
+  const moneda = useFetch(`https://api.coincap.io/v2/assets/${selectedCrypto}/history?interval=h1`);
+  const coins = useFetch('https://api.coincap.io/v2/assets');
+  const rates = useFetch('https://api.coincap.io/v2/rates');
+
   ChartJS.register(
     CategoryScale,
     LinearScale,
@@ -24,6 +36,7 @@ const CoinHistory = ({ idCoin }) => {
     Tooltip,
     Legend,
   );
+
   const options = {
     responsive: true,
     plugins: {
@@ -41,15 +54,7 @@ const CoinHistory = ({ idCoin }) => {
       },
     },
   };
-  // const getTimes = (cryptoCurrency) => {
-  //   const times = [];
-  //   moneda?.data?.data.slice(-24).map((value) => (
-  //     times.push(new Date(value.date).getHours() > 12
-  //       ? `${new Date(value.date).getHours() - 12}: ${new Date(value.date).getMinutes()} PM`
-  //       : `${new Date(value.date).getHours()}: ${new Date(value.date).getMinutes()} AM`)
-  //   ));
-  //   return times;
-  // };
+
   const data = {
     labels: moneda?.data?.data.slice(-24).map((value) => {
       const date = new Date(value.date);
@@ -67,16 +72,39 @@ const CoinHistory = ({ idCoin }) => {
       },
     ],
   };
+
+  const handleChange = (ev) => {
+    setSelectedCrypto(ev.target.value);
+  };
+
+  useEffect(() => {
+    if (!coins.loading && !rates.loading) {
+      setAllCryptos(coins?.data?.data);
+      setAllCoins(rates?.data?.data);
+    }
+  }, [coins, rates]);
+
   return (
-    <div className='col-12 px-4 col-md-8 px-md-0 my-4 mx-auto bg-dark text-light'>
-        <h3 className='text-center'>
-          <i className="bi bi-graph-down-arrow"></i>
-        </h3>
-        <h5 className='text-center'>Grafico de Precios (USD)</h5>
-      <Line options={options} data={data} />
+    <div className={classNames('col-12 col-md-8 p-4 px-md-0 my-4 mx-auto', {
+      'bg-light text-dark': theme === 'light',
+      'bg-dark text-light': theme === 'dark',
+    })}>
+      <h3 className='text-center'><i className='bi bi-graph-down-arrow'></i></h3>
+      <h5 className='text-center'>Grafico de Precios (USD)</h5>
+      <div className='d-flex col-8 mx-auto'>
+        <select className='form-select' onChange={(ev) => handleChange(ev)} aria-label='Cryptocurrency selector'>
+          <option defaultValue>Seleccione una criptomoneda</option>
+          {allCryptos.map(currency => <option key={currency.id}
+                value={currency.id}>{currency.symbol}</option>)}
+        </select>
+      </div>
+      <Line className='p-2' options={options} data={data} />
       <NavLink className='nav-link text-center' to='/'>
-        <button className='btn btn-primary bg-dark border-0'>
-          <i className="bi bi-arrow-left"></i>
+        <button className={classNames('btn', {
+          'btn-light border border-1 border-dark': theme === 'light',
+          'btn-dark': theme === 'dark',
+        })}>
+          <i className='bi bi-arrow-left'></i>
         </button>
       </NavLink>
     </div>
